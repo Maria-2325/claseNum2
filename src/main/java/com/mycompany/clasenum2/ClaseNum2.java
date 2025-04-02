14d3cf66650f3c958761c21ba408633d353470e7
Licet <marialiset13@gmail.com>
2025-04-02 10:46:35 -0500

    Finalizacion del primer programa

---
+++ src/main/java/com/mycompany/clasenum2/ClaseNum2.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.clasenum2;
+
+/**
+ *
+ * @author ibarr
+ */
+public class ClaseNum2 {
+
+    public static void main(String[] args) {
+        /* int contador = 5;
+        //Sintaxis del while
+        while(contador>=5){
+            System.out.println("Hola: "+contador);
+            contador=contador +3;
+        
+        }
+        //Sintaxis del Do while
+        do {
+            System.out.println("Hola: " + contador);
+            contador = contador + 3;
+            contador ++;
+        }while(contador<5);*/
+ /*  System.out.println("Primer ejercicio");
+        //Estructuras secuenciales
+        double base= 2;
+        double altura=4;
+        double area = 0;
+        
+        area = base * altura;
+        System.out.println("El area es: "+ area);*/
+ /*System.out.println("Segundo programa");
+        //Estructuras condicional
+        int num = -2;
+        if (num>0){
+            System.out.println("El numero es positivo:" + num);
+        }
+        else{
+            System.out.println("Error el numero es negativo");
+        }*/
+ /* System.out.println("Tercer programa");
+       //Estructura condicional
+       float num= 5;
+       float operacion = num/2;
+       if (operacion ==0){
+           System.out.println("El valor es par: "+ operacion);
+       }
+       else {
+           System.out.println("El valor es impar");
+       }*/
+        System.out.println("Cuarto programa");
+        //Estructura condicional
+        int nota = -10;
+        if (nota >= 0 && nota <= 100) {
+            if (nota >= 90) {
+                System.out.println("Excelente");
+            } else if (nota >= 70 && nota <= 89) {
+                System.out.println("Aprobado");
+            } else if (nota < 70) {
+                System.out.println("Reprobado");
+
+            }
+        } else {
+            System.out.println("Su nota es invalida");
+
+        }
+
+    }
+}
